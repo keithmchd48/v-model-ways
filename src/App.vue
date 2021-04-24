5cd3cf11a201930680cc8e803c7d4c83957f8124
keithmchd48 <keithmchd48@gmail.com>
2021-04-24 19:54:17 +0530

    Local Variable Watcher

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    1. Local Variable Watcher
+    <LocalVariableWatcher v-model="model1"></LocalVariableWatcher>
+    {{ model1 }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LocalVariableWatcher from "@/components/way1/LocalVariableWatcher";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LocalVariableWatcher,
+  },
+  data() {
+    return {
+      model1: "",
+    };
+  },
+};
 </script>
 
 <style>
@@ -19,7 +26,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/way1/LocalVariableWatcher.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" v-model="model" />
+</template>
+
+<script>
+export default {
+  name: "LocalVariableWatcher",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      model: this.value,
+    };
+  },
+  watch: {
+    model(currentValue) {
+      this.$emit("input", currentValue);
+    },
+  },
+};
+</script>
